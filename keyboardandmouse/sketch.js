174cc7035022a8774ba6ff559b38c2d10e67ac3f
Xyrebot1 <36053918+Xyrebot1@users.noreply.github.com>
2018-02-08 14:53:48 -0600

    test

--- keyboardandmouse/sketch.js
+++ keyboardandmouse/sketch.js
@@ -2,33 +2,43 @@
 // Dan Schellenberg - replace with your name
 // Feb 2, 2018 - replace with the date
 
-// global variables
-let gear;
+// // global variables
+// let gear;
+//
+// // the preload function guarentees that the code inside the function is
+// // executed before the rest of the program runs -- helpful for things
+// // like loading images (since JS is asynchronous)
+// function preload() {
+//   gear = loadImage("images/gear.png");
+// }
+//
+// // the setup function will only run once (before the draw loop begins)
+// // this is where you want to set up the environment (size of canvas, etc)
+// function setup() {
+//   createCanvas(windowWidth, windowHeight);
+// }
+//
+// function draw() {
+//   background(255);
+//
+//   image(gear, 0, 0);
+//
+//   stroke(0);
+//   line(0, 0, 200, 200);
+//
+//   fill(0, 255, 0, 100);
+//   noStroke();
+//
+//   rect(mouseX, mouseY, 100, 300);
+//   ellipse(400, 150, 300, 200);
+// }
+  function setup() {
+    createCanvas(windowWidth,windowHeight);
+  }
 
-// the preload function guarentees that the code inside the function is
-// executed before the rest of the program runs -- helpful for things
-// like loading images (since JS is asynchronous)
-function preload() {
-  gear = loadImage("images/gear.png");
-}
-
-// the setup function will only run once (before the draw loop begins)
-// this is where you want to set up the environment (size of canvas, etc)
-function setup() {
-  createCanvas(windowWidth, windowHeight);
-}
-
-function draw() {
-  background(255);
-
-  image(gear, 0, 0);
-
-  stroke(0);
-  line(0, 0, 200, 200);
-
-  fill(0, 255, 0, 100);
-  noStroke();
-
-  rect(mouseX, mouseY, 100, 300);
-  ellipse(400, 150, 300, 200);
-}
+  function draw() {
+      if (mousePressed(RIGHT)) {
+        fill(random(255)),random(255),random(255));
+        rect(mouseX,mouseY,150,200);
+      }
+  }
