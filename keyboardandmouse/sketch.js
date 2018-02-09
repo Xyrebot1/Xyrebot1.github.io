a60072d25428ea2d5e61b6376c4014bfa364df74
Xyrebot1 <36053918+Xyrebot1@users.noreply.github.com>
2018-02-09 15:19:37 -0600

    Basic set

--- keyboardandmouse/sketch.js
+++ keyboardandmouse/sketch.js
@@ -1,34 +1,34 @@
 // p5js Mouse and Keyboard
 // Dan Schellenberg - replace with your name
 // Feb 2, 2018 - replace with the date
+let sizeX
+let sizeY
+let bGC
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   document.addEventListener("contextmenu", event => event.preventDefault());
+  sizeX = 100;
+  sizeY = 100;
+  bGC = 255;
 }
 
 function draw() {
-
+  
 }
 
 function mousePressed() {
   if (mouseButton === LEFT) {
-    noStroke();
-    fill(random(255), random(255), random(255));
-    rect(mouseX, mouseY, 200, 150);
-  } else if (mouseButton === RIGHT) {
-    noStroke();
-    fill(random(255), random(255), random(255));
-    ellipse(mouseX, mouseY, 150, 150);
+    fill(255,0,0);
+    rect(mouseX - sizeX/2, mouseY - sizeY/2, sizeX, sizeY);
   }
 }
 
 function keyPressed() {
   if (key === 'w' || key === 'W') {
-    background(255);
-  }
-  else if (key === 'b' || key === 'B') {
-    background(0);
+    bGC = 255;
+  } else if (key === 'b' || key === 'B') {
+    bGC = 0;
   }
   if (key === 'r' || key === 'R') {
     background(random(255), random(255), random(255));
