490397e549322730565e77084e29b6893c8b0538
Xyrebot1 <36053918+Xyrebot1@users.noreply.github.com>
2018-03-22 15:21:41 -0600

    BALLS

--- templateProject/sketch.js
+++ templateProject/sketch.js
@@ -2,34 +2,55 @@
 // Dan Schellenberg - replace with your name
 // Feb 2, 2018 - replace with the date
 
-// global variables
-let backgroundMusic;
-let spellSound;
+let circle;
 
-// the preload function guarentees that the code inside the function is
-// executed before the rest of the program runs -- helpful for things
-// like loading images (since JS is asynchronous)
-function preload() {
-  backgroundMusic = loadSound("assets/four_loop.mp3");
-  spellSound = loadSound("assets/spell2.wav")
-}
-
-// the setup function will only run once (before the draw loop begins)
-// this is where you want to set up the environment (size of canvas, etc)
 function setup() {
-  createCanvas(windowWidth, windowHeight);
-  backgroundMusic.setVolume(0.4);
-  backgroundMusic.loop();
-  spellSound.setVolume(0.7);
+  createCanvas(windowWidth,windowHeight);
+  circle = [];
 }
 
 function draw() {
-  // background(255);
+  background(255);
+  noStroke();
+  moveBalls();
+  displayBalls();
 }
 
-function mousePressed() {
-  fill(random(255), random(255), random(255));
-  noStroke();
-  ellipse(random(width), random(height), 50, 50);
-  spellSound.play();
+function moveBalls() {
+  for (let j = 0; j < circle.length; j++) {
+    circle[j].x += circle[j].dX;
+    circle[j].y += circle[j].dY;
+
+    if (circle[j].y < 0) {
+      circle[j].y = height;
+    }
+    else if (circle[j].y > height) {
+      circle[j].y = 0;
+    }
+    if (circle[j].x < 0) {
+      circle[j].x = width;
+    }
+    else if (circle[j].x > width) {
+      circle[j].x = 0;
+    }
+  }
+}
+
+function displayBalls() {
+  for (let i = 0; i < circle.length; i++) {
+    fill(circle[i].colour);
+    ellipse(circle[i].x,circle[i].y,circle[i].size);
+  }
+}
+
+function mouseClicked() {
+  let aBall = {
+    x: mouseX,
+    y: mouseY,
+    size: random(10,100),
+    colour: color(random(255), random(255), random(255), random(255)),
+    dX: random(-5, 5),
+    dY: random(-5, 5),
+  };
+  circle.push(aBall);
 }
