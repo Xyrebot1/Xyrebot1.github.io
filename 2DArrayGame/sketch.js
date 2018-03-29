140313ad4bad31d333255157e2f57f48df04f923
Xyrebot1 <36053918+Xyrebot1@users.noreply.github.com>
2018-03-29 15:21:26 -0600

    RPG?

--- 2DArrayGame/sketch.js
+++ 2DArrayGame/sketch.js
@@ -2,21 +2,26 @@
 // Dan Schellenberg
 // Mar 26, 2018
 
-let rows = 24;
-let cols = 24;
+let rows = 16;
+let cols = 32;
 let grid;
 let cellSize;
+let moveX;
+let moveY;
 
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   cellSize = width / cols;
   grid = createEmpty2dArray(cols, rows);
+  moveX = 0;
+  moveY = 15;
 }
 
 function draw() {
   background(255);
   displayGrid();
+  playerThing();
 }
 
 function displayGrid() {
@@ -24,6 +29,9 @@ function displayGrid() {
     for (let y=0; y<rows; y++) {
       if (grid[x][y] === 0) {
         fill(255);
+      }
+      else  if (grid[x][y] === 2) {
+        fill(255, 0, 255);
       }
       else {
         fill(50);
@@ -33,6 +41,10 @@ function displayGrid() {
   }
 }
 
+function playerThing() {
+  grid[moveX][moveY] = 2;
+}
+
 function mousePressed() {
   let xCoord = floor(mouseX / cellSize);
   let yCoord = floor(mouseY / cellSize);
@@ -40,14 +52,23 @@ function mousePressed() {
   if (grid[xCoord][yCoord] === 1) {
     grid[xCoord][yCoord] = 0;
   }
-  else {
+  else  if (grid[xCoord][yCoord] === 0) {
     grid[xCoord][yCoord] = 1;
   }
+  else {
+    grid[xCoord][yCoord] = 2;
+  }
 }
+
+
 
 function keyPressed() {
   if (key === "c" || key === "C") {
     grid = createEmpty2dArray(cols, rows);
+  }
+  else if (keyCode === DOWN_ARROW && moveY < rows - 1) {
+    grid = createEmpty2dArray(cols, rows);
+    moveY += 1;
   }
 }
 
